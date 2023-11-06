48d49c980c53f0ce71e330b0ac31fbeabe4b39e0
15072006jeferson <141078597+15072006jeferson@users.noreply.github.com>
2023-11-06 19:44:27 -0300

    ok

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -3,6 +3,6 @@ const fora = document.getElementById("novoItem")
 form.addEventListener("submit",(evento) => {
     evento.preventDefault()
 
-    
     console.log("funcionou")
+    console.log(evento.target[0].value)    
 })
