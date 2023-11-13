1ce0216de0ee3dc3bd45455ffed43279bfe5f01d
15072006jeferson <141078597+15072006jeferson@users.noreply.github.com>
2023-11-13 19:34:20 -0300

    arrumei o log

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -3,15 +3,19 @@ const fora = document.getElementById("novoItem")
 form.addEventListener("submit",(evento) => {
     evento.preventDefault()
 
-    console.log  
-    console.log 
-
     criaElemento(elemento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
 })
-
 
 function criaElemento (nome,quantidade) {
     console.log(nome)
     console.log(quantidade)
 
+
+   const novoItem = document.createElement('Li')
+   novoItem.classList.add("item")
+
+const numeroItem = document.createElement('strong')
+numeroItem.innerHTML = quantidade
+
+console.log(numeroItem)
 }
