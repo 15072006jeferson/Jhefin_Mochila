3dff6c26097aa7275881a00aac33c7c2f2d337a6
15072006jeferson <141078597+15072006jeferson@users.noreply.github.com>
2023-11-27 19:09:01 -0300

    ok

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -1,30 +1,37 @@
 const form = document.getElementById("novoItem")
-const lista = document.getElementbyid("lista")
+const lista = document.getElementById("lista")
+const itens = []
 
-form.addEventListener("submit",(evento) => {
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value,elements.value)
+    criaElemento(nome.value, quantidade.value)
 
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento (nome,quantidade) {
-   const novoItem = document.createElement('Li')
-   novoItem.classList.add("item")
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
 
-const numeroItem = document.createElement('strong')
-numeroItem.innerHTML = quantidade
-novoItem.appendChild(numeroItem)
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    novoItem.appendChild(numeroItem)
 
-novoItem.innerHTML += nome
+    novoItem.innerHTML += nome
 
-Lista.appendChild(novoItem)
+    lista.appendChild(novoItem)
 
-localStorage.setItem("nome,nome")
-localStorage.setItem("quantidade,quantidade")
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
